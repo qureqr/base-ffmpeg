f518d283de7cffbe0b8a0b443172527a0c1ae021
СвятоСвят <qureqr@gmail.com>
2024-06-15 01:27:35 +0300

    чутьчуть улучшений

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const path = require('path');
 // Установим путь к ffmpeg
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 
-const inputFilePath = 'editshaheed.mp4';  // Входной видео файл
-const outputFilePath = 'output.mp4'; // Выходной видео файл
+const inputFilePath = '1.mp4';  // Входной видео файл
+const outputFilePath = 'output.mp3'; // Выходной аудио файл
 
 ffmpeg(inputFilePath)
+    .setStartTime('00:00:02') // Установка времени начала обрезки
     .outputOptions([
-        '-vcodec libx264',  // Установка кодека для видео
-        '-crf 28',          // Установка коэффициента сжатия (значение 0-51, где 0 - наилучшее качество, 51 - наихудшее)
-        '-preset fast'      // Установка предустановки сжатия (ultrafast, superfast, veryfast, faster, fast, medium, slow, slower, veryslow)
+        '-vn',              // Отключение видео
+        '-acodec libmp3lame', // Установка кодека для аудио
+        '-q:a 0'            // Установка качества аудио (значение 0-9, где 0 - наилучшее качество)
     ])
     .on('start', (commandLine) => {
         console.log('Started: ' + commandLine);
